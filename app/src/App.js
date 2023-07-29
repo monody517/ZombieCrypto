739d9dd60e3e3ec529171769f8e956e7d2c23778
monody517 <2737716714@qq.com>
2023-07-29 16:39:24 +0800

    change local chain

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,30 @@
-import { useAccount, useConnect, useEnsName } from 'wagmi'
+import { useAccount, useConnect, useEnsName,useBalance } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
  
 function Profile() {
-  const { address, isConnected } = useAccount()
+  const { address, isConnecting, status } = useAccount()
+  const { connector } = useAccount()
   const { data: ensName } = useEnsName({ address })
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
- 
-  if (isConnected) return <div>Connected to {ensName ?? address}</div>
-  return <button onClick={() => connect()}>Connect Wallet</button>
+  const balance = useBalance({
+    addressOrName: address,
+    formatUnits: 'ether',
+  });
+
+  return (
+      <div>
+      <ul>
+        {status === 'connected' ? <li>{status}</li>:<button onClick={() => connect()}>Connect Wallet</button>}
+        <h3>Current connector: {connector?.name || 'None'}</h3>
+        <li>Address: {address}</li>
+        <li>Connecting status: {status}</li>
+        <li>balance: {balance.data?.formatted || '-'}</li>
+      </ul>
+    </div>
+  )
+
 }
 
 export default Profile;
--- app/src/wagmiConfig.ts
+++ app/src/wagmiConfig.ts
@@ -1,8 +1,23 @@
-import { createConfig, configureChains, mainnet } from 'wagmi'
+import { createConfig, configureChains, Chain } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'  
 
+const localChain: Chain = {
+  id: 1337,
+  name: "Local",
+  network: "Local",
+  nativeCurrency: {
+    name: "Noah",
+    symbol: "NOAH",
+    decimals: 18,
+  },
+  rpcUrls: {
+     public: { http: ['http://127.0.0.1:7545'] },
+    default: {http: ["http://127.0.0.1:7545"]},
+  },
+};
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
+  [localChain],
   [publicProvider()],
 )
 
